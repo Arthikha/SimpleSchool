9bef874cb14be9e1707a68c516864342541918a0
Arthikha <arthikha.22@cse.mrt.ac.lk>
2024-09-03 13:46:27 +0530

    initial

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function showAlert() {
+    alert("Welcome to Our School!");
+}
+
+function dropDown() {
+    document.getElementById("admissionDropDown").classList.toggle("show");
+}
+  
+  
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var admissionDropDown = document.getElementById("admissionDropDown");
+        if (admissionDropDown.classList.contains('show')) {
+            admissionDropDown.classList.remove('show');
+        }
+    }
+}
